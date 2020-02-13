831936bbc4e1bf14cd0be132d9168c4ecbe8f35f
Thomas <thomas.klampfl@oeaw.ac.at>
2020-02-13 17:06:06 +0100

    Improvement of the modal dialogues and added more information to them

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -105,8 +105,140 @@ $(document).ready(function(){
        var baseUrl = "showNoTemplate.html?directory=indices&document="
        var url = baseUrl+dataType+"&entiyID="+dataKey;
        $('#loadModal').load(url, function(){
+		   
+		// new code start
+	  let mentionedLink = $('#myModal div div div h3 small a');
+	  if (mentionedLink !== undefined){
+		let reference = $(mentionedLink).attr('href');
+		let pmbKey = 0;
+		if (reference !== undefined){
+			// console.log(reference);
+			let position = reference.lastIndexOf('pmb');
+			pmbKey = reference.substring(position + 3,reference.length);
+		}
+		else{
+			mentionedLink = $('#myModal div div div h4 a'); // this case should not be necessary because the delivered html for the modal was corrected
+			if (mentionedLink !== undefined){
+				let reference = $(mentionedLink).attr('href');
+				if (reference !== undefined){
+					let position = reference.lastIndexOf('pmb');
+					pmbKey = reference.substring(position + 3,reference.length);
+				}
+			}
+		}
+		// console.log(pmbKey);
+		let url = 'https://pmb.acdh.oeaw.ac.at/apis/api2/entity/' + pmbKey + '/?format=json';
+		let promise = $.get(url,function(data){
+			// console.log(data);
+			if (data.entity_type === 'Work'){
+				for (let i = 0; i < data.relations.persons.length; i++){
+					let urlOfPerson = data.relations.persons[i].target.url;
+					if (urlOfPerson !== undefined && data.relations.persons[i].relation_type.label.startsWith('steht in Beziehung zu Person >> geschaffen von')){
+						let secondPromise = $.get(urlOfPerson,function(data){
+							// console.log(data);
+						});
+						secondPromise.then(function(data){
+							let surname = data.name;
+							let firstName = data.first_name;
+							let birth = data.start_date;
+							let death = data.end_date;
+							let idOfPerson = data.id;
+							let linkhref = '/exist/apps/schnitzler-briefe/pages/hits.html?searchkey=pmb' + idOfPerson;
+							$('.modal-body').append('<div><a href="' + linkhref + '">' + firstName + ' ' + surname + '</a></div>');
+							$('.modal-body').append('<div>' + birth + " - " + death + '</div>');
+						});
+					}
+					else{
+						let idOfRelation = data.relations.persons[i].target.id;
+						let labelOfRelation = data.relations.persons[i].relation_type.label;
+						let targetOfRelation = data.relations.persons[i].target.name;
+						let firstname = data.relations.persons[i].target.first_name;
+						let linkhref = '/exist/apps/schnitzler-briefe/pages/hits.html?searchkey=pmb' + idOfRelation;
+						$('.modal-body').append('<div>' + labelOfRelation + ' <a href="' + linkhref + '">' + firstname + " " + targetOfRelation + '</a></div>');
+					}
+				}
+				// places relations
+				for (let i = 0; i < data.relations.places.length; i++){
+					let idOfRelation = data.relations.places[i].target.id;
+					let labelOfRelation = data.relations.places[i].relation_type.label;
+					let targetOfRelation = data.relations.places[i].target.name;
+					let linkhref = '/exist/apps/schnitzler-briefe/pages/hits.html?searchkey=pmb' + idOfRelation;
+					$('.modal-body').append('<div>' + labelOfRelation + ' <a href="' + linkhref + '">' + targetOfRelation + '</a></div>');
+				}
+				// events relations
+				for (let i = 0; i < data.relations.events.length; i++){
+					let idOfRelation = data.relations.events[i].target.id;
+					let labelOfRelation = data.relations.events[i].relation_type.label;
+					let targetOfRelation = data.relations.events[i].target.name;
+					let linkhref = '/exist/apps/schnitzler-briefe/pages/hits.html?searchkey=pmb' + idOfRelation;
+					$('.modal-body').append('<div>' + labelOfRelation + ' <a href="' + linkhref + '">' + targetOfRelation + '</a></div>');
+				}
+				// works relations
+				for (let i = 0; i < data.relations.works.length; i++){
+					let idOfRelation = data.relations.works[i].target.id;
+					let labelOfRelation = data.relations.works[i].relation_type.label;
+					let targetOfRelation = data.relations.works[i].target.name;
+					let linkhref = '/exist/apps/schnitzler-briefe/pages/hits.html?searchkey=pmb' + idOfRelation;
+					$('.modal-body').append('<div>' + labelOfRelation + ' <a href="' + linkhref + '">' + targetOfRelation + '</a></div>');
+				}
+				// institutions relations
+				for (let i = 0; i < data.relations.institutions.length; i++){
+					let idOfRelation = data.relations.institutions[i].target.id;
+					let labelOfRelation = data.relations.institutions[i].relation_type.label;
+					let targetOfRelation = data.relations.institutions[i].target.name;
+					let linkhref = '/exist/apps/schnitzler-briefe/pages/hits.html?searchkey=pmb' + idOfRelation;
+					$('.modal-body').append('<div>' + labelOfRelation + ' <a href="' + linkhref + '">' + targetOfRelation + '</a></div>');
+				}
+			}
+			if (data.entity_type === 'Institution' || data.entity_type === 'Places' || data.entity_type === 'Persons'){
+				// places relations
+				for (let i = 0; i < data.relations.places.length; i++){
+					let idOfRelation = data.relations.places[i].target.id;
+					let labelOfRelation = data.relations.places[i].relation_type.label;
+					let targetOfRelation = data.relations.places[i].target.name;
+					let linkhref = '/exist/apps/schnitzler-briefe/pages/hits.html?searchkey=pmb' + idOfRelation;
+					$('.modal-body').append('<div>' + labelOfRelation + ' <a href="' + linkhref + '">' + targetOfRelation + '</a></div>');
+				}
+				// events relations
+				for (let i = 0; i < data.relations.events.length; i++){
+					let idOfRelation = data.relations.events[i].target.id;
+					let labelOfRelation = data.relations.events[i].relation_type.label;
+					let targetOfRelation = data.relations.events[i].target.name;
+					let linkhref = '/exist/apps/schnitzler-briefe/pages/hits.html?searchkey=pmb' + idOfRelation;
+					$('.modal-body').append('<div>' + labelOfRelation + ' <a href="' + linkhref + '">' + targetOfRelation + '</a></div>');
+				}
+				// works relations
+				for (let i = 0; i < data.relations.works.length; i++){
+					let idOfRelation = data.relations.works[i].target.id;
+					let labelOfRelation = data.relations.works[i].relation_type.label;
+					let targetOfRelation = data.relations.works[i].target.name;
+					let linkhref = '/exist/apps/schnitzler-briefe/pages/hits.html?searchkey=pmb' + idOfRelation;
+					$('.modal-body').append('<div>' + labelOfRelation + ' <a href="' + linkhref + '">' + targetOfRelation + '</a></div>');
+				}
+				// institutions relations
+				for (let i = 0; i < data.relations.institutions.length; i++){
+					let idOfRelation = data.relations.institutions[i].target.id;
+					let labelOfRelation = data.relations.institutions[i].relation_type.label;
+					let targetOfRelation = data.relations.institutions[i].target.name;
+					let linkhref = '/exist/apps/schnitzler-briefe/pages/hits.html?searchkey=pmb' + idOfRelation;
+					$('.modal-body').append('<div>' + labelOfRelation + ' <a href="' + linkhref + '">' + targetOfRelation + '</a></div>');
+				}
+				// persons relations
+				for (let i = 0; i < data.relations.persons.length; i++){
+					let idOfRelation = data.relations.persons[i].target.id;
+					let labelOfRelation = data.relations.persons[i].relation_type.label;
+					let targetOfRelation = data.relations.persons[i].target.name;
+					let firstname = data.relations.persons[i].target.first_name;
+					let linkhref = '/exist/apps/schnitzler-briefe/pages/hits.html?searchkey=pmb' + idOfRelation;
+					$('.modal-body').append('<div>' + labelOfRelation + ' <a href="' + linkhref + '">' + firstname + " " + targetOfRelation + '</a></div>');
+				}
+			}
+		});
+	  }
+	  // new code end   
+		   
        $('#myModal').modal('show');
        });
-   }
+   }   
    });
 });
--- resources/xslt/listperson.xsl
+++ resources/xslt/listperson.xsl
@@ -132,7 +132,7 @@
                                         </tr>
                                     </xsl:if>
                                 </table>
-                                <div>
+                                <!--<div>
                                     <h4 data-toggle="collapse" data-target="#more"> more (tei structure)</h4>
                                     <div id="more" class="collapse">
                                         <xsl:choose>
@@ -145,7 +145,7 @@
                                             </xsl:otherwise>
                                         </xsl:choose>
                                     </div>
-                                </div>
+                                </div>-->
                             </div>
                         </xsl:when>
                         <xsl:otherwise>
@@ -163,7 +163,7 @@
                         </xsl:otherwise>
                     </xsl:choose>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">X</button>
+                        <!--<button type="button" class="btn btn-default" data-dismiss="modal">X</button>-->
                     </div>
                 </div>
             </div>
